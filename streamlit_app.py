97193ce606f2fc2e606aa008320dade5cab9beeb
krnekibb <45263214+krnekibb@users.noreply.github.com>
2026-07-13 13:27:13 +0200

    Restore Smoothiefroot API call in streamlit_app.py
    Re-add the Smoothiefroot API request and display response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,6 @@ session = cnx.session()
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col("FRUIT_NAME"))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
-
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients: ",
@@ -54,3 +51,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_statement).collect()
         st.success(f'Your Smoothie is ordered! {name_on_order}', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response)
